ddcad602e9e797cb61e99034dee0565613f560ea
Madgamermlg <120388991+Madgamermlg@users.noreply.github.com>
2024-05-15 14:22:17 +0000

    Battler ver2.0, bug fixes and updated comperator

--- modules/router.ts
+++ modules/router.ts
@@ -71,7 +71,9 @@ export class Router {
 		app.get("/battler", (req, res) => {
 			res.render("battler");
 		});
-
+		app.get("/mysterybattler", (req, res) => {
+			res.render("mysterybattler");
+		});
 		app.get("/caught", (req, res) => {
 			res.render("caught");
 		});
