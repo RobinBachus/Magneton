1fe01956735ddd64864eb4f69c68bcff4188f148
RobinBachus <robinbachus@gmail.com>
2024-05-29 14:43:10 +0000

    Small changes to signup and small clean up

--- server.ts
+++ server.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import ejs, { render } from "ejs";
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import Database from "./modules/database";
 import CleanUp from "./modules/cleanup";
@@ -11,10 +10,12 @@ import LoginRouter from "./routes/loginRouter";
 import SecureRouter from "./routes/secureRouter";
 import MiscRouter from "./routes/miscRouter";
 import IRouter from "./routes/router";
+import { apiLogger, getPokemonByGen, getPokemonRange } from "./modules/api";
 
 dotenv.config();
 
 let cleanup: CleanUp;
+let database: Database;
 
 async function main(): Promise<StatusCode> {
 	const app = express();
@@ -22,7 +23,6 @@ async function main(): Promise<StatusCode> {
 	app.set("view engine", "ejs");
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
-	app.use(cookieParser("secret"));
 	app.use(express.static("public"));
 	app.use(session);
 
@@ -31,15 +31,17 @@ async function main(): Promise<StatusCode> {
 	const { result, status } = await Database.create();
 	if (failed(status)) return status;
 
-	const database = result!;
+	database = result!;
 
-	cleanup = new CleanUp(database);
+	// cleanup = new CleanUp(database);
 
 	IRouter.db = database;
 
 	app.use(new LoginRouter().router);
 	app.use(new SecureRouter().router);
 	app.use(new MiscRouter().router);
+
+	// apiLogger.log((await getPokemonByGen("gen7")).length);
 
 	app.listen(app.get("port"), () => {
 		Logger.log(`Server running on http://localhost:${app.get("port")}`);
@@ -48,8 +50,20 @@ async function main(): Promise<StatusCode> {
 	return StatusCode.success;
 }
 
-main().then((status) => {
-	if (cleanup) cleanup.exitCode = status;
-});
+main()
+	.then((status) => {
+		if (cleanup) cleanup.exitCode = status;
+	})
+	.catch(async (_) => {
+		if (cleanup) {
+			cleanup.exitCode = StatusCode.failed;
+			return;
+		}
+
+		if (database) await database.close();
+
+		Logger.error("An unexpected error occurred, exiting...");
+		process.exit(StatusCode.failed);
+	});
 
 export {};
